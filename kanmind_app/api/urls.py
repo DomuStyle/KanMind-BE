6ae42f3f4b913764662604c203eb4d5ca6e7d8b4
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-06-04 15:03:13 +0200

    added: new path for BoardDetailView

--- kanmind_app/api/urls.py
+++ kanmind_app/api/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
 
 # local imports
-from .views import BoardListCreateView
+from .views import BoardListCreateView, BoardDetailView
 
 urlpatterns = [
-    path('boards/', BoardListCreateView.as_view(), name='board-list-create'),
+    # link /boards/ endpoint to BoardsListCreateView
+    path('boards/', BoardListCreateView.as_view(), name='boards-list-create'),
+    # link /boards/<board_id>/ endpoint to BoardsDetailView
+    path('boards/<int:board_id>/', BoardDetailView.as_view(), name='boards-detail'),
 ]
